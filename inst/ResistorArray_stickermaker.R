096c30ec679dc79c9a4534f7b4a998694cd87078
Robin Hankin <hankin.robin@gmail.com>
2021-09-19 09:52:56 +1200

    sticker

---
+++ inst/ResistorArray_stickermaker.R
@@ -0,0 +1,8 @@
+library("hexSticker")
+
+sticker("ResistorArray_icon.png", package="ResistorArray",
+        p_size=18, s_x=1, s_y=0.8, s_width=0.8,asp=sqrt(3)/2,
+        white_around_sticker=TRUE, h_fill="#7733FF",
+        h_color="#000000", filename="ResistorArray.png")
+
+
